6bcced90a3a88352a754579fea4e03e32030775b
Gaurav Singh <astog28@gmail.com>
2018-03-03 15:42:02 -0600

    Initial creation of pytorch port

--- numpy/stoch_test.py
+++ numpy/stoch_test.py
@@ -1,10 +1,12 @@
 import numpy as np
-from stoch_util import stochify_vector
+from stoch_util import stochify_vector, stoch_dot, to_real
 
 
-xv = 2 * np.random.rand(5) - 1
-xm = stochify_vector(xv, 16, True, True)
-print("\n=============\n")
-print(xv)
-print("\n=============\n")
-print(xm)
+xv = 2 * np.random.rand(4) - 1
+yv = 2 * np.random.rand(4) - 1
+xyd = np.dot(xv, yv)
+xsm = stochify_vector(xv, 4096, True, True)
+ysm = stochify_vector(yv, 4096, True, True)
+ap_xyd = to_real(stoch_dot(xsm, ysm, True, True, 0), True)
+print(xyd)
+print(ap_xyd)
