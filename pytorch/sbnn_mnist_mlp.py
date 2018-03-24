8b0f3f4fe36ff5af23a955819b84af049abc2ac8
Gaurav Singh <astog28@gmail.com>
2018-03-23 19:52:04 -0500

    Reformated logging statements to be cleaner
    All parameters are not now arguments to script. Prints them at start.

--- pytorch/sbnn_mnist_mlp.py
+++ pytorch/sbnn_mnist_mlp.py
@@ -2,90 +2,57 @@ from __future__ import print_function
 import argparse
 import torch
 import torch.nn as nn
-from utils import printProgressBar
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 from sb_modules import BinarizeLinear, BinaryHardTanhH, binarize
 import time
+import datetime
 # from models.binarized_modules import  Binarize,Ternarize,Ternarize2,Ternarize3,Ternarize4,HingeLoss
 
 # Training settings
-parser = argparse.ArgumentParser(description='Stochastic BNN')
-parser.add_argument('--batch-size', type=int, default=100, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=1000, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--momentum', type=float, default=0.1, metavar='M',
-                    help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=1234, metavar='S',
-                    help='random seed (default: 1234)')
-parser.add_argument('--dpath', type=str, default="./pytorch_data/", metavar='P',
-                    help='path of download folder')
-parser.add_argument('--npasses', type=int, default=8, metavar='S',
-                    help='number of passes to make per forward pass (default: 8)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
+parser = argparse.ArgumentParser(description='Stochastic BNN for MNIST MLP')
+parser.add_argument('--seed', type=int, default=1234)
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--batch-size', type=int, default=100)
+parser.add_argument('--test-batch-size', type=int, default=100)
+parser.add_argument('--hunits', type=int, default=1024)
+parser.add_argument('--npasses', type=int, default=8)
+parser.add_argument('--momentum', type=float, default=0.1)
+parser.add_argument('--dp-in', type=float, default=0.2)
+parser.add_argument('--dp-hidden', type=float, default=0.5)
+parser.add_argument('--epsilon', type=float, default=1e-4)
+parser.add_argument('--dpath', type=str, default="./pytorch_data/")
+parser.add_argument('--download', action='store_true', default=False)
+parser.add_argument('--no-cuda', action='store_true', default=False)
+parser.add_argument('--log-interval', type=int, default=50)
 
 args = parser.parse_args()
+
+for arg in vars(args):
+    print("{0:{1}<20} {2}".format(str(arg) + ' ', '-', getattr(args, arg)))
+print("\n")
+
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-
+# Dataset transformations
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+# Cuda dataset arguments
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(args.dpath, train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
 
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(args.dpath, train=False, transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])),
-    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+train_dataset = datasets.MNIST(args.dpath, train=True, download=args.download, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
 
-
-# BN parameters
-print("batch_size = " + str(args.batch_size))
-# alpha is the exponential moving average factor
-print("alpha = " + str(args.momentum))
-epsilon = 1e-4
-print("epsilon = " + str(epsilon))
-
-# MLP parameters
-num_units = 1024
-print("num_units = " + str(num_units))
-
-# Training parameters
-print("num_epochs = " + str(args.epochs))
-
-# Dropout parameters
-dropout_in = .2  # 0. means no dropout
-print("dropout_in = " + str(dropout_in))
-dropout_hidden = .5
-print("dropout_hidden = " + str(dropout_hidden))
-
-# Decaying LR
-LR_start = .003
-print("LR_start = " + str(LR_start))
-LR_fin = 0.00150
-print("LR_fin = " + str(LR_fin))
-LR_decay = (LR_fin / LR_start)**(1. / args.epochs)
-print("LR_decay = " + str(LR_decay))
-# BTW, LR decay might good for the BN moving average...
-
-print("Number of passes per forward: ", args.npasses, end='\n\n')
+test_dataset = datasets.MNIST(args.dpath, train=False, download=args.download, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
 
 class Net(nn.Module):
@@ -93,24 +60,24 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.hidden_units = hidden_units
 
-        self.dropin = nn.Dropout(dropout_in)
+        self.dropin = nn.Dropout(args.dp_in)
         self.dense1 = BinarizeLinear(input_features, self.hidden_units)
-        self.bn1 = nn.BatchNorm1d(self.hidden_units, epsilon, args.momentum)
+        self.bn1 = nn.BatchNorm1d(self.hidden_units, args.epsilon, args.momentum)
         self.actv1 = BinaryHardTanhH()
-        self.drophidden1 = nn.Dropout(dropout_hidden)
+        self.drophidden1 = nn.Dropout(args.dp_hidden)
 
         self.dense2 = BinarizeLinear(self.hidden_units, self.hidden_units)
-        self.bn2 = nn.BatchNorm1d(self.hidden_units, epsilon, args.momentum)
+        self.bn2 = nn.BatchNorm1d(self.hidden_units, args.epsilon, args.momentum)
         self.actv2 = BinaryHardTanhH()
-        self.drophidden2 = nn.Dropout(dropout_hidden)
+        self.drophidden2 = nn.Dropout(args.dp_hidden)
 
         self.dense3 = BinarizeLinear(self.hidden_units, self.hidden_units)
-        self.bn3 = nn.BatchNorm1d(self.hidden_units, epsilon, args.momentum)
+        self.bn3 = nn.BatchNorm1d(self.hidden_units, args.epsilon, args.momentum)
         self.actv3 = BinaryHardTanhH()
-        self.drophidden3 = nn.Dropout(dropout_hidden)
+        self.drophidden3 = nn.Dropout(args.dp_hidden)
 
         self.dense4 = BinarizeLinear(self.hidden_units, output_features)
-        self.bn4 = nn.BatchNorm1d(output_features, epsilon, args.momentum)
+        self.bn4 = nn.BatchNorm1d(output_features, args.epsilon, args.momentum)
 
     def forward(self, x):
         # Input layer
@@ -159,14 +126,14 @@ class Net(nn.Module):
             self.dense4.bias.data.clamp_(-1, 1)
 
 
-model = Net(28 * 28, 10, num_units)
+model = Net(28 * 28, 10, args.hunits)
 if args.cuda:
     torch.cuda.set_device(0)
     model.cuda()
 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=LR_start)
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
 
 def forward_pass(model, data):
@@ -178,7 +145,7 @@ def forward_pass(model, data):
 def merged_forward_pass(model, data, npasses):
     if npasses >= 2:
         partial_output = forward_pass(model, data)
-        for i in xrange(npasses-2):
+        for i in xrange(npasses - 2):
             partial_output += forward_pass(model, data)
 
     # When adding the partial result, detach it
@@ -189,10 +156,15 @@ def merged_forward_pass(model, data, npasses):
 
 
 def train(epoch):
+    # Initialize batchnorm and dropout layers for training
     model.train()
-    print("({}) LR: {:.2e}".format(epoch, optimizer.param_groups[0]['lr']))
-    train_batch_loss = 0
-    for batch_idx, (data, target) in enumerate(train_loader):
+
+    # Logging variables
+    train_batch_count = 0
+    train_batch_avg_loss = 0
+    train_batch_avg_count = 0
+
+    for batch_idx, (data, target) in enumerate(train_loader, 1):
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
@@ -211,48 +183,87 @@ def train(epoch):
         # 3: Make sure to clip weights / biases between -1, 1.
         model.back_clamp()
 
-        train_batch_loss += float(loss)
-        # printProgressBar((batch_idx + 1) * len(data), len(train_loader.dataset), length=50)
+        train_batch_count += 1
+        train_batch_avg_loss += float(loss)
+        train_batch_avg_count += 1
+
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+            print("Epoch: {: <6}\tBatches: {: 7.2f}%\t Average Batch Loss: {:.5e}".format(
+                epoch, 100. * train_batch_count / len(train_loader),
+                float(loss) / train_batch_avg_count
+            ))
+            train_batch_avg_loss = 0
+            train_batch_avg_count = 0
 
-    # Decay LR
-    optimizer.param_groups[0]['lr'] *= LR_decay
-    print("Train Set - Average batch loss: {:.6f}".format(train_batch_loss / float(len(train_loader))))
+    if train_batch_avg_loss > 0:
+        print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
+            epoch, 100. * train_batch_count / len(train_loader),
+            float(loss) / train_batch_avg_count
+        ))
 
 
-def test():
+def test(epoch):
+    # Initialize batchnorm and dropout layers for testing
     model.eval()
-    test_loss = 0
+
+    # Logging variables
     correct = 0
-    for data, target in test_loader:
+    test_batch_count = 0
+    test_batch_avg_loss = 0
+    test_batch_avg_count = 0
+
+    for batch_idx, (data, target) in enumerate(test_loader, 1):
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
+
         output = merged_forward_pass(model, data, args.npasses)
-        test_loss += criterion(output, target).data[0]  # sum up batch loss
+        loss = criterion(output, target).data[0]  # sum up batch loss
+
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Total loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        test_batch_count += 1
+        test_batch_avg_loss += float(loss)
+        test_batch_avg_count += 1
+
+        if batch_idx % args.log_interval == 0:
+            print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
+                epoch, 100. * test_batch_count / len(test_loader),
+                float(loss) / test_batch_avg_count
+            ))
+            train_batch_avg_loss = 0
+            test_batch_avg_count = 0
+
+    if train_batch_avg_loss > 0:
+        print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
+            epoch, 100. * test_batch_count / len(test_loader),
+            float(loss) / test_batch_avg_count
+        ))
+
+    print('\nTest set accuracy: {}/{} ({:.4f}%)'.format(
+        correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)
+    ))
 
     return correct
 
 
 if __name__ == '__main__':
-    max_correct = 0
+    correct = None
     for epoch in range(1, args.epochs + 1):
         time_start = time.clock()
         train(epoch)
-        print("Finished Training. Running Test Set")
-        max_correct = max(test(), max_correct)
-        time_end = time.clock() - time_start
-        print("Took time {} sec(s)\n".format(time_end))
+        print("\n{:-<72}\n".format(""))
+        correct = test(epoch)
+        time_complete = time.clock() - time_start
+        print("\nTime to complete epoch {} == {} sec(s)".format(
+            epoch, time_complete
+        ))
+        print("Estimated time left == {}".format(
+            str(datetime.timedelta(seconds=time_complete * (args.epochs - epoch)))
+        ))
 
-    print("\n===========================\n\nBEST TEST ACCURACY: {:.4f} - {}/{}\n".format(
-        100. * max_correct / len(test_loader.dataset), max_correct, len(test_loader.dataset)))
+        print("{:=<72}\n".format(""))
+
+    print("\nFinal test accuracy: {:.4f}".format(100. * correct / len(test_loader.dataset), end='\n\n'))
