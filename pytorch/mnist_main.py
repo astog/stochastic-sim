Fixed issues with backprop
Added AMSGrad from lates version of pytorch.

pytorch/mnist_main.py
@@ -6,35 +6,43 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.autograd import Variable
+from Adam import Adam
 
 # Models
 # from sbnn_models.mlp import Net
-from model_archive.lenet5 import Net
+# from model_archive.lenet5 import Net
+from sbnn_models.lenet5 import Net
+# from model_archive.mnist_mlp import Net
 
 import time
 import datetime
+import math
 import numpy as np
+from scipy.stats import norm
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 # Training settings
 parser = argparse.ArgumentParser(description='Stochastic BNN for MNIST MLP')
 parser.add_argument('--seed', type=int, default=1234)
-parser.add_argument('--lr', type=float, default=0.001)
-parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--lr-start', type=float, default=0.001)
+parser.add_argument('--lr-end', type=float, default=0.00001)
+parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--no-shuffle', action='store_true', default=False)
-parser.add_argument('--valid-pcent', type=float, default=0.1)
-parser.add_argument('--batch_size', type=int, default=100)
-parser.add_argument('--hunits', type=int, default=1024)
+parser.add_argument('--valid-pcent', type=float, default=0.15)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--hunits', type=int, default=32)
 parser.add_argument('--npasses', type=int, default=8)
 parser.add_argument('--momentum', type=float, default=0.1)
 parser.add_argument('--dp-hidden', type=float, default=0.5)
-parser.add_argument('--epsilon', type=float, default=1e-4)
+parser.add_argument('--epsilon', type=float, default=1e-6)
 parser.add_argument('--dpath', type=str, default="./pytorch_data/")
 parser.add_argument('--download', action='store_true', default=False)
 parser.add_argument('--no-cuda', action='store_true', default=False)
 parser.add_argument('--no-save', action='store_true', default=False)
 parser.add_argument('--log-interval', type=int, default=50)
 parser.add_argument('--check', type=str, default=None)
+parser.add_argument('--test-count', type=int, default=100)
 
 args = parser.parse_args()
 
@@ -46,6 +54,8 @@ args.cuda = not args.no_cuda and torch.cuda.is_available()
 args.shuffle = not args.no_shuffle
 args.save = not args.no_save
 
+LR_decay = (args.lr_end / args.lr_start)**(1. / args.epochs)
+np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
@@ -66,7 +76,6 @@ indices = list(range(num_train))
 split = int(np.floor(args.valid_pcent * num_train))
 
 if args.shuffle:
-    np.random.seed(args.seed)
     np.random.shuffle(indices)
 
 train_idx, valid_idx = indices[split:], indices[:split]
@@ -78,15 +87,16 @@ valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=args.shuffle, **kwargs)
 
 
-# model = Net(28 * 28, 10, args.hunits, args.npasses, bias=True, dp_hidden=args.dp_hidden, momentum=args.momentum, epsilon=args.epsilon)
-model = Net()
+# model = Net(28 * 28, 10, args.hunits, args.npasses, bias=False, dp_hidden=args.dp_hidden, momentum=args.momentum, epsilon=args.epsilon)
+# model = Net()
+model = Net(args.npasses, momentum=args.momentum, epsilon=args.epsilon)
 if args.cuda:
     torch.cuda.set_device(0)
     model.cuda()
 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
+optimizer = Adam(model.parameters(), lr=args.lr_start, amsgrad=True)
 
 
 def train(epoch):
@@ -136,6 +146,7 @@ def validate(epoch):
     valid_batch_count = 0
     valid_batch_avg_loss = 0
     valid_batch_avg_count = 0
+    val_loss = 0
 
     for batch_idx, (data, target) in enumerate(valid_loader, 1):
         if args.cuda:
@@ -150,6 +161,7 @@ def validate(epoch):
 
         valid_batch_count += 1
         valid_batch_avg_loss += float(loss)
+        val_loss += float(loss)
         valid_batch_avg_count += 1
 
         if batch_idx % args.log_interval == 0:
@@ -167,11 +179,11 @@ def validate(epoch):
         ))
 
     print('\nValidation set accuracy: {}/{} ({:.4f}%)'.format(
-        correct, len(valid_loader)*args.batch_size,
-        100. * (float(correct) / (len(valid_loader)*args.batch_size))
+        correct, len(valid_loader) * args.batch_size,
+        100. * (float(correct) / (len(valid_loader) * args.batch_size))
     ))
 
-    return correct
+    return correct, val_loss
 
 
 def test(epoch):
@@ -214,42 +226,72 @@ def test(epoch):
         ))
 
     print('\nTest set accuracy: {}/{} ({:.4f}%)'.format(
-        correct, len(test_loader)*args.batch_size,
-        100. * (float(correct) / (len(test_loader)*args.batch_size))
+        correct, len(test_loader) * args.batch_size,
+        100. * (float(correct) / (len(test_loader) * args.batch_size))
     ))
 
     return correct
+
+
+def adam_set_lr(optimizer, epoch):
+    for param in optimizer.param_groups:
+        param['lr'] = args.lr_start / pow(epoch, 1.0/4.0)
 
 
 if __name__ == '__main__':
     print("Training batches:", len(train_loader))
     print("Validation batches:", len(valid_loader))
     print("Test batches:", len(test_loader), end='\n\n')
+    save_model_path = "{}-{}.mkl".format("model", int(time.time()))
 
     if args.check is not None:
         print("Loading module", args.check)
         model.load_state_dict(torch.load(args.check))
-        train(0)
-        validate(0)
-        test(0)
+
+        test_accuracy = np.zeros(args.test_count)
+        for itest in xrange(args.test_count):
+            test_accuracy[itest] = (float(test(itest)) / (len(test_loader) * args.batch_size))
+
+        # best fit of data
+        (mu, sigma) = norm.fit(test_accuracy)
+
+        # the histogram of the data
+        n, bins, patches = plt.hist(test_accuracy, 60, normed=1, facecolor='green', alpha=0.75)
+
+        # add a 'best fit' line
+        y = mlab.normpdf(bins, mu, sigma)
+        l = plt.plot(bins, y, 'r--', linewidth=2)
+
+        # plot
+        plt.xlabel('Smarts')
+        plt.ylabel('Probability')
+        plt.title(r'$\mathrm{Histogram\ of\ Test accuracy:}\ \mu=%.3f,\ \sigma=%.3f$' % (mu, sigma))
+        plt.grid(True)
+
+        plt.show()
     else:
         max_valid_correct = 0
         test_correct = 0
+        # scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=LR_decay)
         for epoch in range(1, args.epochs + 1):
             time_start = time.clock()
             train(epoch)
+            # scheduler.step(epoch=epoch)
+            adam_set_lr(optimizer, epoch)
             print("\n{:-<72}".format(""))
             print("Validation:\n")
-            correct = validate(epoch)
+            correct, val_loss = validate(epoch)
             if correct > max_valid_correct:
                 max_valid_correct = correct
                 print("\n{:-<72}".format(""))
                 print("Test:\n")
                 test_correct = test(epoch)
+                if args.save:
+                    torch.save(model.state_dict(), save_model_path)
             else:
                 print('\nBest Validation set accuracy: {}/{} ({:.4f}%)'.format(
-                    max_valid_correct, len(valid_loader)*args.batch_size,
-                    100. * (float(max_valid_correct) / (len(valid_loader)*args.batch_size))
+                    max_valid_correct, len(valid_loader) * args.batch_size,
+                    100. * (float(max_valid_correct) / (len(valid_loader) * args.batch_size))
                 ))
 
             time_complete = time.clock() - time_start
@@ -263,11 +305,6 @@ if __name__ == '__main__':
             print("{:=<72}\n".format(""))
 
         print('\nFinal Test set accuracy: {}/{} ({:.4f}%)'.format(
-            test_correct, len(test_loader)*args.batch_size,
-            100. * (float(test_correct) / (len(test_loader)*args.batch_size))
+            test_correct, len(test_loader) * args.batch_size,
+            100. * (float(test_correct) / (len(test_loader) * args.batch_size))
         ))
-
-        if args.save:
-            st = int(time.time())
-            save_model_path = "{}-{}.mkl".format("model", st)
-            torch.save(model.state_dict(), save_model_path)

pytorch/model_archive/mnist_mlp.py
@@ -1,324 +1,71 @@
 from __future__ import print_function
-import argparse
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data.sampler import SubsetRandomSampler
-from torch.autograd import Variable
-import time
-import datetime
-import numpy as np
-
-# Training settings
-parser = argparse.ArgumentParser(description='Stochastic BNN for MNIST MLP')
-parser.add_argument('--seed', type=int, default=1234)
-parser.add_argument('--lr', type=float, default=0.001)
-parser.add_argument('--epochs', type=int, default=1000)
-parser.add_argument('--no-shuffle', action='store_true', default=False)
-parser.add_argument('--valid-pcent', type=float, default=0.1)
-parser.add_argument('--batch-size', type=int, default=100)
-parser.add_argument('--hunits', type=int, default=1024)
-parser.add_argument('--momentum', type=float, default=0.1)
-parser.add_argument('--dp-in', type=float, default=0.2)
-parser.add_argument('--dp-hidden', type=float, default=0.5)
-parser.add_argument('--epsilon', type=float, default=1e-4)
-parser.add_argument('--dpath', type=str, default="./pytorch_data/")
-parser.add_argument('--download', action='store_true', default=False)
-parser.add_argument('--no-cuda', action='store_true', default=False)
-parser.add_argument('--log-interval', type=int, default=50)
-
-args = parser.parse_args()
-
-for arg in vars(args):
-    print("{0:{1}<20} {2}".format(str(arg) + ' ', '-', getattr(args, arg)))
-print("\n")
-
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-args.shuffle = not args.no_shuffle
-
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
-
-# Dataset transformations
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-])
-# Cuda dataset arguments
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_dataset = datasets.MNIST(args.dpath, train=True, download=args.download, transform=transform)
-valid_dataset = datasets.MNIST(args.dpath, train=True, download=args.download, transform=transform)
-test_dataset = datasets.MNIST(args.dpath, train=False, download=args.download, transform=transform)
-
-num_train = len(train_dataset)
-indices = list(range(num_train))
-split = int(np.floor(args.valid_pcent * num_train))
-
-if args.shuffle:
-    np.random.seed(args.seed)
-    np.random.shuffle(indices)
-
-train_idx, valid_idx = indices[split:], indices[:split]
-train_sampler = SubsetRandomSampler(train_idx)
-valid_sampler = SubsetRandomSampler(valid_idx)
-
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, sampler=train_sampler, **kwargs)
-valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, sampler=valid_sampler, **kwargs)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=args.shuffle, **kwargs)
+import torch.nn.functional as F
 
 
 class Net(nn.Module):
-    def __init__(self, input_features, output_features, hidden_units):
+    def __init__(self, input_features, output_features, hidden_units, npasses, bias=False, dp_hidden=0.5, momentum=0.1, epsilon=1e-6):
         super(Net, self).__init__()
-        self.hidden_units = hidden_units
+        self.npasses = npasses
+        self.input_features = input_features
+        self.training_epoch = 0
 
-        self.dropin = nn.Dropout(args.dp_in)
-        self.dense1 = nn.Linear(input_features, self.hidden_units)
-        self.bn1 = nn.BatchNorm1d(self.hidden_units, args.epsilon, args.momentum)
-        self.actv1 = nn.ReLU()
-        self.drophidden1 = nn.Dropout(args.dp_hidden)
+        self.dense1 = nn.Linear(input_features, hidden_units, bias=bias)
+        self.bn1 = nn.BatchNorm1d(hidden_units, epsilon, momentum)
+        self.drophidden1 = nn.Dropout(dp_hidden)
 
-        self.dense2 = nn.Linear(self.hidden_units, self.hidden_units)
-        self.bn2 = nn.BatchNorm1d(self.hidden_units, args.epsilon, args.momentum)
-        self.actv2 = nn.ReLU()
-        self.drophidden2 = nn.Dropout(args.dp_hidden)
+        self.dense2 = nn.Linear(hidden_units, hidden_units, bias=bias)
+        self.bn2 = nn.BatchNorm1d(hidden_units, epsilon, momentum)
+        self.drophidden2 = nn.Dropout(dp_hidden)
 
-        self.dense3 = nn.Linear(self.hidden_units, self.hidden_units)
-        self.bn3 = nn.BatchNorm1d(self.hidden_units, args.epsilon, args.momentum)
-        self.actv3 = nn.ReLU()
-        self.drophidden3 = nn.Dropout(args.dp_hidden)
+        self.dense3 = nn.Linear(hidden_units, hidden_units, bias=bias)
+        self.bn3 = nn.BatchNorm1d(hidden_units, epsilon, momentum)
+        self.drophidden3 = nn.Dropout(dp_hidden)
 
-        self.dense4 = nn.Linear(self.hidden_units, output_features)
-        self.bn4 = nn.BatchNorm1d(output_features, args.epsilon, args.momentum)
+        self.dense4 = nn.Linear(hidden_units, output_features, bias=bias)
+        self.bn4 = nn.BatchNorm1d(output_features, epsilon, momentum)
 
-    def forward(self, x):
+    def fpass(self, x):
         # Input layer
-        x = x.view(-1, 28 * 28)
-        x = self.dropin(x)
+        x = x.view(-1, self.input_features)
 
         # 1st hidden layer
         x = self.dense1(x)
-        x = self.bn1(x)
-        x = self.actv1(x)
+        x = F.tanh(self.bn1(x))
         x = self.drophidden1(x)
 
         # 2nd hidden layer
         x = self.dense2(x)
-        x = self.bn2(x)
-        x = self.actv2(x)
+        x = F.tanh(self.bn2(x))
         x = self.drophidden2(x)
 
         # 3nd hidden layer
         x = self.dense3(x)
-        x = self.bn3(x)
-        x = self.actv3(x)
+        x = F.tanh(self.bn3(x))
         x = self.drophidden3(x)
 
         # Output Layer
         x = self.dense4(x)
         x = self.bn4(x)
 
+        # Logits output. coz of pytorch CrossEntropy has a builin log-softmax
         return x
+
+    def forward(self, x):
+        return self.fpass(x)
+
+    def backward(self, loss, optimizer):
+        # Zero grads, before calculation
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
+
+        # Clamp weights between [-1, 1]
+        self.back_clamp()
 
     def back_clamp(self):
         for module in self.modules():
             if isinstance(module, nn.Linear):
-                module.weight.data.clamp_(-1, 1)
-                if module.bias is not None:
-                    module.bias.data.clamp_(-1, 1)
-
-
-model = Net(28 * 28, 10, args.hunits)
-if args.cuda:
-    torch.cuda.set_device(0)
-    model.cuda()
-
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
-
-def train(epoch):
-    # Initialize batchnorm and dropout layers for training
-    model.train()
-
-    # Logging variables
-    train_batch_count = 0
-    train_batch_avg_loss = 0
-    train_batch_avg_count = 0
-
-    for batch_idx, (data, target) in enumerate(train_loader, 1):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data), Variable(target)
-        optimizer.zero_grad()
-
-        output = model(data)
-
-        loss = criterion(output, target)
-
-        optimizer.zero_grad()
-        loss.backward()
-
-        # 2: Do optimization step
-        optimizer.step()
-
-        # 3: Make sure to clip weights / biases between -1, 1.
-        model.back_clamp()
-
-        train_batch_count += 1
-        train_batch_avg_loss += float(loss)
-        train_batch_avg_count += 1
-
-        if batch_idx % args.log_interval == 0:
-            print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
-                epoch, 100. * train_batch_count / len(train_loader),
-                train_batch_avg_loss / train_batch_avg_count
-            ))
-            train_batch_avg_loss = 0
-            train_batch_avg_count = 0
-
-    if train_batch_avg_count > 0:
-        print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
-            epoch, 100. * train_batch_count / len(train_loader),
-            train_batch_avg_loss / train_batch_avg_count
-        ))
-
-
-def validate(epoch):
-    # Initialize batchnorm and dropout layers for testing
-    model.eval()
-
-    # Logging variables
-    correct = 0
-    valid_batch_count = 0
-    valid_batch_avg_loss = 0
-    valid_batch_avg_count = 0
-
-    for batch_idx, (data, target) in enumerate(valid_loader, 1):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-
-        output = model(data)
-        loss = criterion(output, target).data[0]  # sum up batch loss
-
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-        valid_batch_count += 1
-        valid_batch_avg_loss += float(loss)
-        valid_batch_avg_count += 1
-
-        if batch_idx % args.log_interval == 0:
-            print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
-                epoch, 100. * valid_batch_count / len(valid_loader),
-                valid_batch_avg_loss / valid_batch_avg_count
-            ))
-            valid_batch_avg_loss = 0
-            valid_batch_avg_count = 0
-
-    if valid_batch_avg_count > 0:
-        print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
-            epoch, 100. * valid_batch_count / len(valid_loader),
-            float(loss) / valid_batch_avg_count
-        ))
-
-    print('\nValidation set accuracy: {}/{} ({:.4f}%)'.format(
-        correct, len(valid_loader)*args.batch_size,
-        100. * (float(correct) / (len(valid_loader)*args.batch_size))
-    ))
-
-    return correct
-
-
-def test(epoch):
-    # Initialize batchnorm and dropout layers for testing
-    model.eval()
-
-    # Logging variables
-    correct = 0
-    test_batch_count = 0
-    test_batch_avg_loss = 0
-    test_batch_avg_count = 0
-
-    for batch_idx, (data, target) in enumerate(test_loader, 1):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-
-        output = model(data)
-        loss = criterion(output, target).data[0]  # sum up batch loss
-
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-        test_batch_count += 1
-        test_batch_avg_loss += float(loss)
-        test_batch_avg_count += 1
-
-        if batch_idx % args.log_interval == 0:
-            print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
-                epoch, 100. * test_batch_count / len(test_loader),
-                test_batch_avg_loss / test_batch_avg_count
-            ))
-            test_batch_avg_loss = 0
-            test_batch_avg_count = 0
-
-    if test_batch_avg_count > 0:
-        print("Epoch: {: <6}\tBatches: {: 7.2f}%\tAverage Batch Loss: {:.5e}".format(
-            epoch, 100. * test_batch_count / len(test_loader),
-            float(loss) / test_batch_avg_count
-        ))
-
-    print('\nTest set accuracy: {}/{} ({:.4f}%)'.format(
-        correct, len(test_loader)*args.batch_size,
-        100. * (float(correct) / (len(test_loader)*args.batch_size))
-    ))
-
-    return correct
-
-
-if __name__ == '__main__':
-    print("Training batches:", len(train_loader))
-    print("Validation batches:", len(valid_loader))
-    print("Test batches:", len(test_loader), end='\n\n')
-    max_valid_correct = 0
-    test_correct = 0
-    for epoch in range(1, args.epochs + 1):
-        time_start = time.clock()
-        train(epoch)
-        print("\n{:-<72}".format(""))
-        print("Validation:\n")
-        correct = validate(epoch)
-        if correct > max_valid_correct:
-            max_valid_correct = correct
-            print("\n{:-<72}".format(""))
-            print("Test:\n")
-            test_correct = test(epoch)
-        else:
-            print('\nBest Validation set accuracy: {}/{} ({:.4f}%)'.format(
-                max_valid_correct, len(valid_loader)*args.batch_size,
-                100. * (float(max_valid_correct) / (len(valid_loader)*args.batch_size))
-            ))
-
-        time_complete = time.clock() - time_start
-        print("\nTime to complete epoch {} == {} sec(s)".format(
-            epoch, time_complete
-        ))
-        print("Estimated time left == {}".format(
-            str(datetime.timedelta(seconds=time_complete * (args.epochs - epoch)))
-        ))
-
-        print("{:=<72}\n".format(""))
-
-    print('\nFinal Test set accuracy: {}/{} ({:.4f}%)'.format(
-        test_correct, len(test_loader)*args.batch_size,
-        100. * (float(test_correct) / (len(test_loader)*args.batch_size))
-    ))
-
-    st = int(time.time())
-    save_model_path = "{}-{}.mkl".format("model", st)
-    torch.save(model.state_dict(), save_model_path)
+                module.weight.data.clamp(-1, 1)

pytorch/sbnn_models/mlp.py
@@ -1,50 +1,54 @@
 from __future__ import print_function
 import torch
 import torch.nn as nn
-from modules import BinarizedLinear, bhtanh
+from modules import BinarizedLinear, BinarizedHardTanH
 
 
 class Net(nn.Module):
-    def __init__(self, input_features, output_features, hidden_units, npasses, bias=False, dp_hidden=0.5, momentum=0.1, epsilon=1e-6):
+    def __init__(self, input_features, output_features, hidden_units, npasses, bias=False, dp_in=0.2, dp_hidden=0.5, momentum=0.1, epsilon=1e-6):
         super(Net, self).__init__()
         self.npasses = npasses
         self.input_features = input_features
+        self.dropin = nn.Dropout(dp_in)
 
-        self.dense1 = BinarizedLinear(input_features, hidden_units, bias=bias)
+        self.dense1 = BinarizedLinear(input_features, hidden_units, bias=bias, deterministic=False)
         self.bn1 = nn.BatchNorm1d(hidden_units, epsilon, momentum)
         self.drophidden1 = nn.Dropout(dp_hidden)
 
-        self.dense2 = BinarizedLinear(hidden_units, hidden_units, bias=bias)
+        self.dense2 = BinarizedLinear(hidden_units, hidden_units, bias=bias, deterministic=False)
         self.bn2 = nn.BatchNorm1d(hidden_units, epsilon, momentum)
         self.drophidden2 = nn.Dropout(dp_hidden)
 
-        self.dense3 = BinarizedLinear(hidden_units, hidden_units, bias=bias)
+        self.dense3 = BinarizedLinear(hidden_units, hidden_units, bias=bias, deterministic=False)
         self.bn3 = nn.BatchNorm1d(hidden_units, epsilon, momentum)
         self.drophidden3 = nn.Dropout(dp_hidden)
 
-        self.dense4 = BinarizedLinear(hidden_units, output_features, bias=bias)
+        self.dense4 = BinarizedLinear(hidden_units, output_features, bias=bias, deterministic=False)
         self.bn4 = nn.BatchNorm1d(output_features, epsilon, momentum)
+
+        self.bhtanh = BinarizedHardTanH()
 
     def fpass(self, x):
         # Input layer
         x = x.view(-1, self.input_features)
+        x = self.dropin(x)
 
         # 1st hidden layer
         x = self.dense1(x)
         x = self.bn1(x)
-        x = bhtanh(x)
+        x = self.bhtanh(x)
         x = self.drophidden1(x)
 
         # 2nd hidden layer
         x = self.dense2(x)
         x = self.bn2(x)
-        x = bhtanh(x)
+        x = self.bhtanh(x)
         x = self.drophidden2(x)
 
         # 3nd hidden layer
         x = self.dense3(x)
         x = self.bn3(x)
-        x = bhtanh(x)
+        x = self.bhtanh(x)
         x = self.drophidden3(x)
 
         # Output Layer
@@ -55,23 +59,23 @@ class Net(nn.Module):
     def forward(self, x):
         # Save weights before binarization
         self.save_weights()
-        if self.npasses >= 2:
-            partial_output = self.fpass(x)
-            for i in xrange(self.npasses - 2):
-                partial_output += self.fpass(x)
 
-            # When adding the partial result, detach it
-            # This ensure that functional graph from the previous passes does not get autograd
-            # Only one backward pass for npasses of forward pass
-            output = (self.fpass(x) + partial_output.detach()) / self.npasses
-            return output
+        accumulated_sum = self.fpass(x)
+        for i in xrange(self.npasses - 1):
+            accumulated_sum += self.fpass(x)
 
-        return self.fpass(x)
+        # Note: Don't detach graph for n-1 passes, since pytorch will calculate
+        # gradients for those passes and then accumulate them.
+        # To maintain the Monte Carlo method of forward and backprop we need
+        # gradients from each forward pass.
+        output = accumulated_sum / float(self.npasses)
 
-    def backward(self, loss, optimizer):
-        # Restore real weights before doing backprop (so that gradients are calculated accurately)
+        # Restore real weights before leaving
         self.restore_weights()
 
+        return output
+
+    def backward(self, loss, optimizer):
         # Zero grads, before calculation
         optimizer.zero_grad()
         loss.backward()
@@ -94,4 +98,6 @@ class Net(nn.Module):
     def back_clamp(self):
         for module in self.modules():
             if isinstance(module, BinarizedLinear):
-                module.weight.data.clamp(-1, 1)
+                module.weight.data.clamp_(-1.0, 1.0)
+                # weight  --> copies into --> real_weight before forward pass
+                # module.real_weight.clamp_(-1.0, 1.0)

pytorch/sbnn_models/modules.py
@@ -6,6 +6,10 @@ import stoch_util as sb
 import warnings
 
 
+# Set to true if want to use BNN. Note npasses has to be 1 though
+using_bnn = False
+
+
 def binarize_(tensor, deterministic=False):
     if deterministic:
         tensor.sign_()
@@ -16,20 +20,23 @@ def binarize_(tensor, deterministic=False):
 
 
 class BinarizedLinear(nn.Linear):
-    def __init__(self, input_features, output_features, bias=False):
+    def __init__(self, input_features, output_features, bias=False, deterministic=False):
+        self.deterministic = deterministic
         # Have the bias as a parameter to make it identitical to other Linear modules. Raise warning if used though
-        if bias:
-            warnings.warn("Bias is deprecated. Running with no bias", DeprecationWarning)
-            bias = False
+        # if bias:
+        #     warnings.warn("Bias is deprecated. Running with no bias", DeprecationWarning)
+        #     bias = False
 
         super(BinarizedLinear, self).__init__(input_features, output_features, bias)
+
+        nn.init.xavier_uniform(self.weight.data, gain=nn.init.calculate_gain('tanh'))
+
         self.real_weight = self.weight.data.clone()
 
     def forward(self, input):
         self.weight.data.copy_(self.real_weight)
-        binarize_(self.weight.data)
-
-        return F.linear(input, self.weight)
+        binarize_(self.weight.data, deterministic=self.deterministic)
+        return F.linear(input, self.weight, self.bias)
 
     def save_param(self):
         # print(self.real_weight, self.weight.data)
@@ -40,20 +47,63 @@ class BinarizedLinear(nn.Linear):
         self.weight.data.copy_(self.real_weight)
 
 
-class BinarizedHardTanH(torch.autograd.Function):
+class BinarizedConv2d(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=False, deterministic=False):
+        super(BinarizedConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.deterministic = deterministic
+
+        nn.init.xavier_uniform(self.weight.data, gain=nn.init.calculate_gain('tanh'))
+
+        self.real_weight = self.weight.data.clone()
+
+    def forward(self, input):
+        self.weight.data.copy_(self.real_weight)
+        binarize_(self.weight.data, deterministic=self.deterministic)
+        out = nn.functional.conv2d(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        return out
+
+    def save_param(self):
+        # print(self.real_weight, self.weight.data)
+        self.real_weight.copy_(self.weight.data)
+
+    def restore_param(self):
+        # print(self.weight.data, self.real_weight)
+        self.weight.data.copy_(self.real_weight)
+
+
+class STHardTanH(torch.autograd.Function):
+    def __init__(self):
+        super(STHardTanH, self).__init__()
+
     @staticmethod
-    def forward(ctx, input):
-        ctx.org_input = input.clone()
-        binarize_(input, deterministic=True)
-        return input
+    def forward(ctx, input, deterministic):
+        ctx.org_input = input
+        ctx.deterministirc = deterministic
+        output = input.clone()
+        # output.tanh()
+        binarize_(output, deterministic=deterministic)
+        return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        # print("Doing backward pass on ACTIVATION")
-        # Straight through estimator
+        # Get data
+        input = ctx.org_input
+        deterministic = ctx.deterministirc
         grad_input = grad_output.clone()
-        grad_input[ctx.org_input.abs() > 1] = 0
-        return grad_input, None
+
+        # Straight through estimator
+        grad_input[input.abs() > 1] = 0
+        # Scale by dtanh/dx
+        # scaling_factor = 1.0 - input.tanh().pow(2.0)
+        # grad_input.data *= scaling_factor
+
+        return grad_input, None, None
 
 
-bhtanh = BinarizedHardTanH.apply
+class BinarizedHardTanH(nn.Module):
+    def __init__(self, deterministic=True):
+        super(BinarizedHardTanH, self).__init__()
+        self.deterministic = deterministic
+
+    def forward(self, input):
+        return STHardTanH.apply(input, self.deterministic)