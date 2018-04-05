12c7bfd37120c20fab1964f6281413437eb5319d
singh431@umn.edu <singh431@umn.edu>
2018-04-05 00:12:24 -0500

    Using cross validation for mnist
    Cleaned up print statements

--- pytorch/dataloader.py
+++ pytorch/dataloader.py
@@ -1,12 +1,12 @@
-from torch.utils.data.dataset import random_split, ConcatDataset
+import torch.utils.data.dataset as torch_data
 
 
-class KFoldDataLoader():
-    def __init__(self, kfolds, dataset):
+class KFoldDataset():
+    def __init__(self, dataset, kfolds):
         split_length = len(dataset) / kfolds
         splits = [split_length] * kfolds
 
-        self.data_subsets = random_split(dataset, splits)
+        self.data_subsets = torch_data.random_split(dataset, splits)
         self.kfolds = kfolds
 
     def get_datasets(self, fold):
@@ -15,4 +15,4 @@ class KFoldDataLoader():
         # Create a new dataset without the fold dataset (held for validation)
         train_set = [subset for idx, subset in enumerate(self.data_subsets) if idx != fold]
         val_set = self.data_subsets[fold]
-        return ConcatDataset(train_set), val_set
+        return torch_data.ConcatDataset(train_set), val_set
